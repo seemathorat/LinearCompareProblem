9a2350bc1fde1a652c592097c36d86e0a3213815
seema thorat <seemagade97@gmail.com>
2021-02-22 14:41:48 +0530

    calculate lenght of Line

--- LineCompare.java
+++ LineCompare.java
@@ -1,10 +1,13 @@
-public class LineCompare
-
+public class  LineCompare
 {
-
-public static void main (String args[])
-{
-  System.out.println("Welcome Message");
-}
+	public static void main(String[] args){
+	int x1=3;
+	int y1=4;
+	int x2=5;
+	int y2=6;
+   double length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);	
+	
+	System.out.println("length of the line is : "+length);
+	}
 }
 
